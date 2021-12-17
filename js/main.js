6c7417dc57d8419c2762c15fa4045756e375184c
Edgar-Meza <edgar.m.dev@gmail.com>
2021-12-17 13:24:54 -0600

    se termino diseño para escritorio

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function padreSelect() {
+    console.log('hhh');
+    document.getElementById('start').style.backgroundColor = "transparent";
+    document.getElementById('start').style.color = '#FFFFFF';
+    document.getElementById('start').style.border = '1px solid #FFFFFF';
+}
+function removeStyle() {
+    document.getElementById('start').removeAttribute('style');
+}
